b63ae2f983acf1746b80e3b8e19b16b6cc6ccbfc
RKoitsalu <raimond.koitsalu@gmail.com>
2014-10-20 13:41:38 +0300

    static view for enrollment activity

--- emmaDashJs.js
+++ emmaDashJs.js
@@ -5,4 +5,79 @@
     	$(this).tab('show')
   	});
 
+
+	$(document).ready(function(){
+
+			var enrolldata = [49, 0,106,129,144,176,135,148,216,194,95,54];
+			var unenrolldata = [0, 25, 98, 93, 106, 84, 105, 104, 91, 83, 106, 92];
+			var totalenroll = 0;
+			var totalunenroll = 0;
+			$.each(enrolldata, function(index, value){
+				totalenroll+=value;
+			});
+			$.each(unenrolldata, function(index, value){
+				totalunenroll+=value;
+			});
+			$('.total-enrollments').prepend(totalenroll);
+			$('.total-unenrollments').prepend(totalunenroll);
+		    $('#container').highcharts({
+		        chart: {
+		            type: 'column'
+		        },
+		        title: {
+		            text: 'Enrollment history'
+		        },
+		        subtitle: {
+		            text: 'Course name'
+		        },
+		        xAxis: {
+		            categories: [
+		                'Jan',
+		                'Feb',
+		                'Mar',
+		                'Apr',
+		                'May',
+		                'Jun',
+		                'Jul',
+		                'Aug',
+		                'Sep',
+		                'Oct',
+		                'Nov',
+		                'Dec'
+		            ]
+		        },
+		        yAxis: {
+		            min: 0,
+		            title: {
+		                text: 'Enroll/Unenroll'
+		            }
+		        },
+		        tooltip: {
+		            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+		            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+		                '<td style="padding:0"><b>{point.y}</b></td></tr>',
+		            footerFormat: '</table>',
+		            shared: true,
+		            useHTML: true
+		        },
+		        plotOptions: {
+		            column: {
+		                pointPadding: 0.1,
+		                borderWidth: 1
+		            }
+		        },
+		        series: [{
+		            name: 'Enroll',
+		            data: enrolldata
+
+		        }, {
+		            name: 'Unenroll',
+		            data: unenrolldata
+
+		        }]
+		    });
+		
+	});
+
 })(jQuery);
+
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+$endpoint = '';
+$username = '';
+$password = '';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,6 +24,15 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script src="emmaDashJs.js"></script>
+        <script src="Highcharts/js/highcharts.js"></script>
+    <script src="Highcharts/js/modules/exporting.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   </head>
   <body>
       
@@ -52,10 +70,8 @@
                 <div class="col-sm-10">
                   <select class="form-control">
                     <option>Enrollment Activity</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Login - Logut activity</option>
+                    <option>Page views</option>                    
                   </select>
                 </div>
               </div>
@@ -72,20 +88,35 @@
                 </div>
               </div>
             </form>
-          </div>
+          
+
+          <div class="row">STATIC VIEW</div>
+          <div class="chart-container"></div>
+            <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+            <br>
+            <div class="row">
+              <div class="chart-description">
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">Enroll - Unenroll</h3>
+                  </div>
+                  <div class="panel-body">
+                    <div class="total-enrollments"> new students have enrolled to course</div>
+                    <div class="total-unenrollments"> students have unenrolled from course</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
           <div class="tab-pane" id="cs">
             <h1>Course Statistics</h1>
+
           </div>          
         </div>
       </div>
 
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-    <script src="emmaDashJs.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
   </body>
 </html>
